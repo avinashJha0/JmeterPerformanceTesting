429958676eedc1e330deddea472d29ebbb195c3a
Avinash-jha-QA <avinash@liv-ex.com>
2023-04-17 20:24:02 +0530

    Forth Commit

--- apache-jmeter-5.5/bin/PluginsManagerCMD.bat
+++ apache-jmeter-5.5/bin/PluginsManagerCMD.bat
@@ -1,3 +1,3 @@
 @echo off
 
-java "-Djava.awt.headless=true" %JVM_ARGS% -jar "%~dp0\..\lib\cmdrunner-2.3.jar" --tool org.jmeterplugins.repository.PluginManagerCMD %*
+java %JVM_ARGS% -jar %~dp0\..\lib\cmdrunner-2.0.jar --tool org.jmeterplugins.repository.PluginManagerCMD %*
